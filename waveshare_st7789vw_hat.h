cc56efd836ac48914369eefa9d48753501ba03a1
antiero <antonynasce@gmail.com>
2021-02-25 17:00:31 +0000

    Add pin settings for Adafruit 1.3 TFT Bonnet 240x240

--- waveshare_st7789vw_hat.h
+++ waveshare_st7789vw_hat.h
@@ -1,6 +1,7 @@
 #pragma once
 
 // Data specific to WaveShare 240x240, 1.3inch IPS LCD ST7789VW hat, https://www.waveshare.com/w/upload/a/ae/ST7789_Datasheet.pdf
+// NOTE: MODIFIED FOR THE  Adafruit 1.3" TFT BONNET (ST7789V 240x240)
 #ifdef WAVESHARE_ST7789VW_HAT
 
 #if !defined(GPIO_TFT_DATA_CONTROL)
@@ -8,11 +9,11 @@
 #endif
 
 #if !defined(GPIO_TFT_BACKLIGHT)
-#define GPIO_TFT_BACKLIGHT 24
+#define GPIO_TFT_BACKLIGHT 26
 #endif
 
 #if !defined(GPIO_TFT_RESET_PIN)
-#define GPIO_TFT_RESET_PIN 27
+#define GPIO_TFT_RESET_PIN 24
 #endif
 
 #endif
